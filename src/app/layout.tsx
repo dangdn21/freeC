d13ad7cff746e9eb0a53bececbf74bab404af290
DangDn <doanngocdang96@gmail.com>
2025-08-12 10:16:00 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,6 +41,7 @@ export default function RootLayout({
           </header>
           <main className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
             {children}
+            <h1>Hello world</h1>
           </main>
         </div>
       </body>
